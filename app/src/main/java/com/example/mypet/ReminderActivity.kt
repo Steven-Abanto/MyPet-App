a9e95e61a72ad101834b3b6ebb21795bd86c8e95
Steven-Abanto <steven.abanto@gmail.com>
2026-02-25 21:03:46 -0500

    Recordatorios añadidos

--- app/src/main/java/com/example/mypet/ReminderActivity.kt
+++ app/src/main/java/com/example/mypet/ReminderActivity.kt
@@ -2,13 +2,26 @@ package com.example.mypet
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.button.MaterialButton
 
 class ReminderActivity : AppCompatActivity() {
+    private lateinit var lvReminders : ListView
+    private var listaReminders = mutableListOf<String>()
+    private lateinit var listAdapter : ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +32,68 @@ class ReminderActivity : AppCompatActivity() {
             insets
         }
         setupBottomNavigation()
+        lvReminders = findViewById(R.id.lvReminders)
+
+        listAdapter = ArrayAdapter(
+            this, //Activity donde se va a ejeutar
+            android.R.layout.simple_list_item_1, //Diseño reutilizado para cada elemento
+            listaReminders //Lista con datos
+        )
+
+        lvReminders.adapter = listAdapter
+
+        val ivAddReminder = findViewById<ImageView>(R.id.ivAddReminder)
+
+        ivAddReminder.setOnClickListener {
+            val dialogView = layoutInflater.inflate(R.layout.modal_add_reminder, null)
+            val dialog = AlertDialog.Builder(this)
+                .setView(dialogView)
+                .create()
+            dialog.show()
+            true
+
+            val etReminder = dialogView.findViewById<EditText>(R.id.etReminder)
+            etReminder.text.clear()
+
+            val btnCancelarReminder = dialogView.findViewById<Button>(R.id.btnCancelarReminder)
+            btnCancelarReminder.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            val btnGuardarReminder = dialogView.findViewById<Button>(R.id.btnGuardarReminder)
+            btnGuardarReminder.setOnClickListener {
+                etReminder.text.trim()
+                if (etReminder.text.isNotEmpty()) {
+                    val reminder = etReminder.text.toString()
+                    listaReminders.add(reminder)
+                    listAdapter.notifyDataSetChanged()
+                    dialog.dismiss()
+                }
+            }
+
+            lvReminders.setOnItemLongClickListener { _, _, position, _ ->
+                val itemReminder = listaReminders[position]
+
+                val dialogView = layoutInflater.inflate(R.layout.modal_options, null)
+                val tvTitulo = dialogView.findViewById<TextView>(R.id.tvTitulo)
+                tvTitulo.text = "Opciones para $itemReminder"
+
+                val dialog = AlertDialog.Builder(this)
+                    .setView(dialogView)
+                    .create()
+                dialog.show()
+
+                val btnEliminar = dialogView.findViewById<MaterialButton>(R.id.mbEliminar)
+                btnEliminar.setOnClickListener {
+                    listaReminders.removeAt(position)
+                    listAdapter.notifyDataSetChanged()
+                    Toast.makeText(this,"Recordatorio eliminado: $itemReminder", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+
+                true
+            }
+        }
     }
 
     private fun setupBottomNavigation() {
